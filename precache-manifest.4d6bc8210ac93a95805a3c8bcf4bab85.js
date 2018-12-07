c96ec7b20f1df4c75d06ea9571207806dc0f6fb3
emilio ovalles-misterman <emilio@emilios-mbp.middlebury.edu>
2018-12-07 00:53:18 -0500

    Updates

---
+++ precache-manifest.4d6bc8210ac93a95805a3c8bcf4bab85.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0305ddaa7e019536a8c9317533f34e9",
+    "url": "/adventure_time/static/media/cocaine.c0305dda.svg"
+  },
+  {
+    "revision": "0ddc1aae5c60c3700b61",
+    "url": "/adventure_time/static/css/main.9cafb92f.chunk.css"
+  },
+  {
+    "revision": "89296c7f91cef5d1153f",
+    "url": "/adventure_time/static/js/1.89296c7f.chunk.js"
+  },
+  {
+    "revision": "7fc58b9458fd32f175ec",
+    "url": "/adventure_time/static/js/runtime~main.7fc58b94.js"
+  },
+  {
+    "revision": "019ca2cc77dc2f93a85cb9433bf56d56",
+    "url": "/adventure_time/static/media/pills.019ca2cc.svg"
+  },
+  {
+    "revision": "27967b2cb8a1fd92aa7149e6fad05dd6",
+    "url": "/adventure_time/static/media/background2.27967b2c.svg"
+  },
+  {
+    "revision": "7e1c04e2aad8597fcc00d7c68fef9241",
+    "url": "/adventure_time/static/media/bong.7e1c04e2.svg"
+  },
+  {
+    "revision": "c1693814106095ab06a181665fc95802",
+    "url": "/adventure_time/static/media/cigs.c1693814.svg"
+  },
+  {
+    "revision": "0ddc1aae5c60c3700b61",
+    "url": "/adventure_time/static/js/main.0ddc1aae.chunk.js"
+  },
+  {
+    "revision": "31d05b91edde30a2d873db243c8a7c9b",
+    "url": "/adventure_time/static/media/heroin.31d05b91.svg"
+  },
+  {
+    "revision": "f300fe6840bd2ee2f14f8d9b028b7e22",
+    "url": "/adventure_time/static/media/rehab.f300fe68.svg"
+  },
+  {
+    "revision": "d20aa364082be30c04ace096c6ad9946",
+    "url": "/adventure_time/static/media/snorter.d20aa364.svg"
+  },
+  {
+    "revision": "dc0680886e55264e70f09ccff35c7b7e",
+    "url": "/adventure_time/static/media/water.dc068088.svg"
+  },
+  {
+    "revision": "41abc6c0f7562ddc1dc39f76cfccdf4b",
+    "url": "/adventure_time/static/media/weights.41abc6c0.svg"
+  },
+  {
+    "revision": "46a2e26b02588a8445410f2376d34cc1",
+    "url": "/adventure_time/static/media/yoga.46a2e26b.svg"
+  },
+  {
+    "revision": "b3e8e943ba102317f2f9713f8df16d1c",
+    "url": "/adventure_time/static/media/coffee.b3e8e943.svg"
+  },
+  {
+    "revision": "55313b4aca251e0e8f6f57b7d574f991",
+    "url": "/adventure_time/index.html"
+  }
+];
